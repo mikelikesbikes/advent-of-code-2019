7438860dd134d0c4a7c315b3570144edb8dd2d05
Mike Busch <mbusch@heroku.com>
2019-12-04 11:49:49 -0500

    refactor day 4

--- day4/day_4.go
+++ day4/day_4.go
@@ -8,50 +8,35 @@ import (
 )
 
 func ValidPassword(pw string) bool {
-	if increasing(pw) && hasRepeat(pw) {
-		return true
-	}
-	return false
+	return increasing(pw) && hasRepeat(pw)
 }
 
 func ValidPassword2(pw string) bool {
-	if increasing(pw) && hasStandaloneDouble(pw) {
-		return true
-	}
-	return false
+	return increasing(pw) && hasStandaloneDouble(pw)
 }
 
 func hasRepeat(pw string) bool {
-	if pw[0] == pw[1] ||
+	return pw[0] == pw[1] ||
 		pw[1] == pw[2] ||
 		pw[2] == pw[3] ||
 		pw[3] == pw[4] ||
-		pw[4] == pw[5] {
-		return true
-	}
-	return false
+		pw[4] == pw[5]
 }
 
 func hasStandaloneDouble(pw string) bool {
-	if (pw[0] == pw[1] && pw[1] != pw[2]) ||
+	return (pw[0] == pw[1] && pw[1] != pw[2]) ||
 		(pw[1] == pw[2] && pw[0] != pw[1] && pw[2] != pw[3]) ||
 		(pw[2] == pw[3] && pw[1] != pw[2] && pw[3] != pw[4]) ||
 		(pw[3] == pw[4] && pw[2] != pw[3] && pw[4] != pw[5]) ||
-		(pw[4] == pw[5] && pw[3] != pw[4]) {
-		return true
-	}
-	return false
+		(pw[4] == pw[5] && pw[3] != pw[4])
 }
 
 func increasing(pw string) bool {
-	if pw[0] <= pw[1] &&
+	return pw[0] <= pw[1] &&
 		pw[1] <= pw[2] &&
 		pw[2] <= pw[3] &&
 		pw[3] <= pw[4] &&
-		pw[4] <= pw[5] {
-		return true
-	}
-	return false
+		pw[4] <= pw[5]
 }
 
 func atoi(s string) int {
@@ -64,8 +49,8 @@ func atoi(s string) int {
 
 func main() {
 	input := "138241-674034"
-	if arg1 := os.Args[1]; arg1 != "" {
-		input = arg1
+	if len(os.Args) > 1 {
+		input = os.Args[1]
 	}
 	minmax := strings.Split(input, "-")
 	if len(minmax) != 2 {
